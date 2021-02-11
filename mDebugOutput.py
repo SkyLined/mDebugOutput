7efcea8006a09272edd587cd916afeb2d8168f4c
SkyLined <github@skylined.nl>
2021-02-11 13:19:44 +0100

    Move __init__ to mDebugOutput

---
+++ mDebugOutput.py
@@ -0,0 +1,25 @@
+from .cCallStack import cCallStack;
+from .fConsoleOutputExceptionDetails import fConsoleOutputExceptionDetails;
+from .fEnableAllDebugOutput import fEnableAllDebugOutput;
+from .fEnableDebugOutputForClass import fEnableDebugOutputForClass;
+from .fEnableDebugOutputForModule import fEnableDebugOutputForModule;
+from .fShowDebugOutput import fShowDebugOutput;
+from .fsToString import fsToString;
+from .fTerminateWithConsoleOutput import fTerminateWithConsoleOutput;
+from .fTerminateWithDeadlock import fTerminateWithDeadlock;
+from .fTerminateWithException import fTerminateWithException;
+from .ShowDebugOutput import ShowDebugOutput;
+
+__all__ = [
+  "cCallStack",
+  "fConsoleOutputExceptionDetails",
+  "fEnableAllDebugOutput",
+  "fEnableDebugOutputForClass",
+  "fEnableDebugOutputForModule",
+  "fShowDebugOutput",
+  "fsToString",
+  "fTerminateWithConsoleOutput",
+  "fTerminateWithDeadlock",
+  "fTerminateWithException",
+  "ShowDebugOutput",
+];
