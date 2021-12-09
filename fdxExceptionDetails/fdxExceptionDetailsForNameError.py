cf5c478bd7634538e33212dcba2441ce3f657cdd
SkyLined <github@skylined.nl>
2021-12-09 13:52:45 +0100

    Use `NameError.name` property instead of a reg.exp. to get variable name.

--- fdxExceptionDetails/fdxExceptionDetailsForNameError.py
+++ fdxExceptionDetails/fdxExceptionDetailsForNameError.py
@@ -3,10 +3,12 @@ import re;
 def fdxExceptionDetailsForNameError(oException):
   if len(oException.args) < 1:
     return {};
-  oUnknownVariableErrorMessageMatch = re.match(r"^(?:global )?name '([_\w]+)' is not defined$", oException.args[0]);
-  if not oUnknownVariableErrorMessageMatch:
-    return {};
-  sVariableName = oUnknownVariableErrorMessageMatch.group(1);
+  sVariableName = oException.name;
+  # Old code used in Python 2 to get the name from the error message:
+  # oUnknownVariableErrorMessageMatch = re.match(r"^(?:global )?name '([_\w]+)' is not defined$", oException.args[0]);
+  # if not oUnknownVariableErrorMessageMatch:
+  #   return {};
+  # sVariableName = oUnknownVariableErrorMessageMatch.group(1);
   return {
     "aasConsoleOutputLines": [
       [
@@ -17,6 +19,7 @@ def fdxExceptionDetailsForNameError(oException):
     ],
     "dxHiddenProperties": {
       "args": oException.args,
+      "name": oException.name,
       "with_traceback": oException.with_traceback,
     },
   };
