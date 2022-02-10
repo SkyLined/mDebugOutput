69c8c37b28ccd926dd03a1cfb928643f55d0366e
SkyLined <github@skylined.nl>
2022-02-10 20:37:57 +0100

    fShowDebugOutput now also takes 2 arguments
    Makes it easier to determine the right class.

--- fShowDebugOutput.py
+++ fShowDebugOutput.py
@@ -5,12 +5,18 @@ from .fTerminateWithException import fTerminateWithException;
 from .mGlobals import *;
 guExitCodeInternalError = 1; # This is the common default I use. There's currently no good way to set this per application.
 
-def fShowDebugOutput(sMessage):
+def fShowDebugOutput(oObject_or_sMessage, s0Message = None):
+  if s0Message is not None:
+    sMessage = s0Message;
+    o0Object = oObject_or_sMessage;
+  else:
+    sMessage = oObject_or_sMessage;
+    o0Object = None;
   try:
     o0ActiveFrame = cFrame.fo0ForCurrentThreadAndFunctionCaller();
     assert o0ActiveFrame, \
         "You cannot use fDebugOutput if all functions on the stack are hidden!";
-    if fbIsDebugOutputEnabledForSourceFilePathAndClass(o0ActiveFrame.sSourceFilePath, o0ActiveFrame.cClass):
+    if fbIsDebugOutputEnabledForSourceFilePathAndClass(o0ActiveFrame.sSourceFilePath, o0Object.__class__ if o0Object else o0ActiveFrame.cClass):
       fDebugOutputHelper(
         o0ActiveFrame.u0ThreadId, o0ActiveFrame.s0ThreadName,
         o0ActiveFrame.sSourceFilePath, o0ActiveFrame.uLastExecutedLineNumber,
@@ -18,4 +24,3 @@ def fShowDebugOutput(sMessage):
       );
   except Exception as oException:
     fTerminateWithException(oException, guExitCodeInternalError);
-
