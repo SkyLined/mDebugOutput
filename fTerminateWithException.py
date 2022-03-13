a5a1e27472925967737c7cbcc0e89c85fbca09fa
SkyLined <github@skylined.nl>
2022-03-13 21:37:53 +0100

    ;

--- fTerminateWithException.py
+++ fTerminateWithException.py
@@ -1,7 +1,7 @@
 import os;
 
 def fTerminateWithException(oException, uExitCode, a0asAdditionalConsoleOutputLines = None, bShowStacksForAllThread = False):
-  fConsoleOutputExceptionDetails(oException, a0asAdditionalConsoleOutputLines = None, bShowStacksForAllThread = False)
+  fConsoleOutputExceptionDetails(oException, a0asAdditionalConsoleOutputLines = None, bShowStacksForAllThread = False);
   os._exit(uExitCode);
 
 from .fConsoleOutputExceptionDetails import fConsoleOutputExceptionDetails;
