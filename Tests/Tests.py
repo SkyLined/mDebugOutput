75b8f5834c3658888fc311b62accb9460e02e6c5
SkyLined <cojmpjum_github.com@sxd.be>
2022-12-08 17:47:43 +0100

    Implement --automatically-fix-dependencies

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -3,7 +3,8 @@ sModulePath = os.path.dirname(__file__);
 sys.path = [sModulePath] + [sPath for sPath in sys.path if sPath.lower() != sModulePath.lower()];
 
 from fTestDependencies import fTestDependencies;
-fTestDependencies();
+fTestDependencies("--automatically-fix-dependencies" in sys.argv);
+sys.argv = [s for s in sys.argv if s != "--automatically-fix-dependencies"];
 
 # I should add some more tests here.
 from fTestExceptionMessages import fTestExceptionMessages;
