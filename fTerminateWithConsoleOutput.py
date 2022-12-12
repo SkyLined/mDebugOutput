833cf3baf75cf2b26ca51b48c61d9e3010811a3c
SkyLined <cojmpjum_github.com@sxd.be>
2022-12-12 10:37:50 +0100

    Fix `fTerminate*` functions

--- fTerminateWithConsoleOutput.py
+++ fTerminateWithConsoleOutput.py
@@ -2,7 +2,17 @@ import os;
 
 from .fConsoleOutput import fConsoleOutput;
 
-def fTerminateWithConsoleOutput(sTitle, aasConsoleOutputLines, uExitCode = 3):
-  fConsoleOutput(sTitle, aasConsoleOutputLines);
+def fTerminateWithConsoleOutput(
+  sTitle,
+  aasConsoleOutputLines,
+  uExitCode = 0, # assume there is no error
+  bPauseBeforeExit = False,
+):
+  fConsoleOutput(
+    sTitle,
+    aasConsoleOutputLines,
+  );
+  if bPauseBeforeExit:
+    input("Press any key to exit...");
   os._exit(uExitCode);
 
--- fTerminateWithException.py
+++ fTerminateWithException.py
@@ -1,7 +1,19 @@
 import os;
 
-def fTerminateWithException(oException, uExitCode, a0asAdditionalConsoleOutputLines = None, bShowStacksForAllThread = False):
-  fConsoleOutputExceptionDetails(oException, a0asAdditionalConsoleOutputLines = None, bShowStacksForAllThread = False);
+def fTerminateWithException(
+  oException,
+  uExitCode = 1, # default for internal exceptions in all my scripts.
+  a0asAdditionalConsoleOutputLines = None,
+  bShowStacksForAllThread = False,
+  bPauseBeforeExit = False,
+):
+  fConsoleOutputExceptionDetails(
+    oException,
+    a0asAdditionalConsoleOutputLines = a0asAdditionalConsoleOutputLines,
+    bShowStacksForAllThread = bShowStacksForAllThread,
+  );
+  if bPauseBeforeExit:
+    input("Press any key to exit...");
   os._exit(uExitCode);
 
 from .fConsoleOutputExceptionDetails import fConsoleOutputExceptionDetails;
